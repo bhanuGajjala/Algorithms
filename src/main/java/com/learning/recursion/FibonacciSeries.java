a31790afd58de8bca4dc402db4988b8f029f776e
bhanuGajjala <bhanukrig@gmail.com>
2020-09-12 23:32:30 -0700

    add fib classes

--- src/main/java/com/learning/recursion/FibonacciSeries.java
+++ src/main/java/com/learning/recursion/FibonacciSeries.java
@@ -1,4 +1,31 @@
 package com.learning.recursion;
 
 public class FibonacciSeries {
+
+    public int fib(int input){
+
+        if(input == 1){
+            return 1;
+        }
+
+        return input * fib(input-1);
+
+    }
+
+    public int add(int input){
+
+        if(input == 1){
+            return 1;
+        }
+
+        return input + add(input -1);
+
+    }
+
+    public int addNumbers(int x, int y) {
+        if(x == 0)
+            return y;
+        else
+            return addNumbers(x - 1,  x + y);
+    }
 }
--- src/test/java/com/learning/recursion/FibonacciSeriesTest.java
+++ src/test/java/com/learning/recursion/FibonacciSeriesTest.java
@@ -1,4 +1,44 @@
 package com.learning.recursion;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FibonacciSeriesTest {
+
+    FibonacciSeries fibonacciSeries;
+
+    @BeforeEach
+    public void setUp() {
+        fibonacciSeries = new FibonacciSeries();
+    }
+
+    @Test
+    public void addTest() {
+
+        int expected = 15;
+        int actual = fibonacciSeries.add(5);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addNumbersTest() {
+
+        int expected = 17;
+        int actual = fibonacciSeries.addNumbers(5, 2);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void fibTest() {
+
+        int expected = 120;
+        int actual = fibonacciSeries.fib(5);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
